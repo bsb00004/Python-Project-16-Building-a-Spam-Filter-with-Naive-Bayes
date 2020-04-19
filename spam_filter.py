8a09e15895850266f89209bd947b86311c52f948
Bikramjit Bhullar <32780436+bsb00004@users.noreply.github.com>
2020-04-19 06:11:15 +0100

    Update spam_filter.py

--- spam_filter.py
+++ spam_filter.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Python Project 16: Building a Spam Filter with Naive Bayes
-# In this project, we're going to build a spam filter for SMS messages using the multinomial Naive Bayes algorithm. Our goal is to write a program that classifies new messages with an accuracy greater than 80% — so we expect that more than 80% of the new messages will be classified correctly as spam or ham (non-spam).
-# 
-# To train the algorithm, we'll use a dataset of 5,572 SMS messages that are already classified by humans. The dataset was put together by Tiago A. Almeida and José María Gómez Hidalgo, and it can be downloaded from the [The UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/datasets/sms+spam+collection). The data collection process is described in more details on [this page](http://www.dt.fee.unicamp.br/~tiago/smsspamcollection/#composition), where you can also find some of the papers authored by Tiago A. Almeida and José María Gómez Hidalgo.
-# 
+### Python Project 16: Building a Spam Filter with Naive Bayes
+# In this project, we're going to build a spam filter for SMS messages using the multinomial Naive Bayes algorithm. 
+# Our goal is to write a program that classifies new messages with an accuracy greater than 80% — 
+# so we expect that more than 80% of the new messages will be classified correctly as spam or ham (non-spam).
+
 # ### Exploring the Dataset
 # We'll now start by reading in the dataset: "`SMSSpamCollection`"
-
-# In[1]:
-
 
 import pandas as pd
 # The data points are tab separated, so we'll need to use the sep='\t' parameter
@@ -22,22 +19,12 @@ sms_spam = pd.read_csv('SMSSpamCollection', sep='\t', header=None, names=['Label
 print(sms_spam.shape)
 sms_spam.head()
 
-
 # Lets find what percentage of the messages is spam and what percentage is ham ("ham" means non-spam).
-
-# In[2]:
-
-
 sms_spam['Label'].value_counts(normalize=True)
 
-
-# Above, we see that 87% of the messages are ham, and the remaining 13% are spam. This sample looks representative, since in practice most messages that people receive are ham.
-# 
-# ### Training and Test Set
-# We're now going to split our dataset into a training and a test set, where the training set accounts for 80% of the data, and the test set for the remaining 20%.
-
-# In[3]:
-
+#### Training and Test Set
+# We're now going to split our dataset into a training and a test set, where the training set accounts for 80% of the data, 
+# and the test set for the remaining 20%.
 
 # Randomize the dataset
 data_randomized = sms_spam.sample(frac=1, random_state=1)
@@ -52,54 +39,25 @@ test_set = data_randomized[training_test_index:].reset_index(drop=True)
 print(training_set.shape)
 print(test_set.shape)
 
-
-# We'll now analyze the percentage of spam and ham messages in the training and test sets. We expect the percentages to be close to what we have in the full dataset, where about 87% of the messages are ham, and the remaining 13% are spam
-
-# In[4]:
-
+# We'll now analyze the percentage of spam and ham messages in the training and test sets. 
 
 training_set['Label'].value_counts(normalize=True)
-
-
-# In[5]:
-
-
 test_set['Label'].value_counts(normalize=True)
 
-
 # The results look good! We'll now move on to cleaning the dataset.
-# 
-# ### Data Cleaning
-# To calculate all the probabilities required by the algorithm, we'll first need to perform a bit of data cleaning to bring the data in a format that will allow us to extract easily all the information we need.
-# 
-# Essentially, we want to bring data to this format:
-# <img, src="https://camo.githubusercontent.com/27a4a0a699bd8f0713d73347abe2929c267a03d5/68747470733a2f2f64712d636f6e74656e742e73332e616d617a6f6e6177732e636f6d2f3433332f637067705f646174617365745f332e706e67">
-# 
-# Letter Case and Punctuation
+
+#### Data Cleaning
 # We'll begin with removing all the punctuation and bringing every letter to lower case.
-
-# In[6]:
-
-
 # Before cleaning
 training_set.head()
-
-
-# In[7]:
-
 
 # After cleaning
 training_set['SMS'] = training_set['SMS'].str.replace('\W', ' ')
 training_set['SMS'] = training_set['SMS'].str.lower()
 training_set.head()
 
-
-# ### Creating the Vocabulary
+#### Creating the Vocabulary
 # Let's now move to creating the vocabulary, which in this context means a list with all the unique words in our training set.
-
-# In[8]:
-
-
 training_set['SMS'] = training_set['SMS'].str.split()
 
 vocabulary = []
@@ -108,69 +66,31 @@ for sms in training_set['SMS']:
         vocabulary.append(word)
         
 vocabulary = list(set(vocabulary))
-
-
-# In[9]:
-
-
 len(vocabulary)
-
-
 # It looks like there are 7,783 unique words in all the messages of our training set.
 
-# ### The Final Training Set
+#### The Final Training Set
 # We're now going to use the vocabulary we just created to make the data transformation we want.
-
-# In[10]:
-
-
 word_counts_per_sms = {unique_word: [0] * len(training_set['SMS']) for unique_word in vocabulary}
 
 for index, sms in enumerate(training_set['SMS']):
     for word in sms:
         word_counts_per_sms[word][index] += 1
 
-
-# In[11]:
-
-
 word_counts = pd.DataFrame(word_counts_per_sms)
 word_counts.head()
 
-
-# Concatenating the DataFrame we just built above with the DataFrame containing the training set (this way, we'll also have the `Label` and the `SMS` columns). Use the `pd.concat()` function.
-
-# In[12]:
-
-
+# Concatenating the DataFrame we just built above with the DataFrame containing the training set 
+# (this way, we'll also have the `Label` and the `SMS` columns). Use the `pd.concat()` function.
 training_set_clean = pd.concat([training_set, word_counts], axis=1)
 training_set_clean.head()
 
 
-# ### Calculating Constants First
-# We're now done with cleaning the training set, and we can begin creating the spam filter. The Naive Bayes algorithm will need to answer these two probability questions to be able to classify new messages:
-# 
-# $$
-# P(Spam | w_1,w_2, ..., w_n) \propto P(Spam) \cdot \prod_{i=1}^{n}P(w_i|Spam)
-# $$$$
-# P(Ham | w_1,w_2, ..., w_n) \propto P(Ham) \cdot \prod_{i=1}^{n}P(w_i|Ham)
-# $$
-# Also, to calculate P(wi|Spam) and P(wi|Ham) inside the formulas above, we'll need to use these equations:
-# 
-# $$
-# P(w_i|Spam) = \frac{N_{w_i|Spam} + \alpha}{N_{Spam} + \alpha \cdot N_{Vocabulary}}
-# $$$$
-# P(w_i|Ham) = \frac{N_{w_i|Ham} + \alpha}{N_{Ham} + \alpha \cdot N_{Vocabulary}}
-# $$
-# Some of the terms in the four equations above will have the same value for every new message. We can calculate the value of these terms once and avoid doing the computations again when a new messages comes in. Below, we'll use our training set to calculate:
-# 
+#### Calculating Constants First
+# We're now done with cleaning the training set, and we can begin creating the spam filter. 
 # - P(Spam) and P(Ham)
 # - NSpam, NHam, NVocabulary
-# 
 # We'll also use Laplace smoothing and set $\alpha = 1$.
-
-# In[13]:
-
 
 # Isolating spam and ham messages first
 spam_messages = training_set_clean[training_set_clean['Label'] == 'spam']
@@ -194,20 +114,9 @@ n_vocabulary = len(vocabulary)
 # Laplace smoothing
 alpha = 1
 
-
-# ### Calculating Parameters
-# Now that we have the constant terms calculated above, we can move on with calculating the parameters $P(w_i|Spam)$ and $P(w_i|Ham)$. Each parameter will thus be a conditional probability value associated with each word in the vocabulary.
-# 
-# The parameters are calculated using the formulas:
-# 
-# $$
-# P(w_i|Spam) = \frac{N_{w_i|Spam} + \alpha}{N_{Spam} + \alpha \cdot N_{Vocabulary}}
-# $$$$
-# P(w_i|Ham) = \frac{N_{w_i|Ham} + \alpha}{N_{Ham} + \alpha \cdot N_{Vocabulary}}
-# $$
-
-# In[14]:
-
+#### Calculating Parameters
+# Calculating the parameters P(w_i|Spam) and P(w_i|Ham). 
+# Each parameter will thus be a conditional probability value associated with each word in the vocabulary.
 
 # Initiate parameters
 parameters_spam = {unique_word:0 for unique_word in vocabulary}
@@ -224,9 +133,9 @@ for word in vocabulary:
     parameters_ham[word] = p_word_given_ham
 
 
-# ### Classifying A New Message
-# Now that we have all our parameters calculated, we can start creating the spam filter. The spam filter can be understood as a function that:
-# 
+#### Classifying A New Message
+# Now that we have all our parameters calculated, we can start creating the spam filter. 
+# The spam filter can be understood as a function that:
 # - Takes in as input a new message (w1, w2, ..., wn).
 # - Calculates P(Spam|w1, w2, ..., wn) and P(Ham|w1, w2, ..., wn).
 # - Compares the values of P(Spam|w1, w2, ..., wn) and P(Ham|w1, w2, ..., wn), and:
@@ -234,16 +143,11 @@ for word in vocabulary:
 #     - If P(Ham|w1, w2, ..., wn) < P(Spam|w1, w2, ..., wn), then the message is classified as spam.
 #     - If P(Ham|w1, w2, ..., wn) = P(Spam|w1, w2, ..., wn), then the algorithm may request human help.
 
-# In[15]:
-
-
 import re
-
 def classify(message):
     '''
     message: a string
     '''
-    
     message = re.sub('\W', ' ', message)
     message = message.lower().split()
     
@@ -266,28 +170,16 @@ def classify(message):
         print('Label: Spam')
     else:
         print('Equal proabilities, have a human classify this!')
-
-
-# In[16]:
-
-
+        
+        
+# Testing above function (Test 1)
 classify('WINNER!! This is the secret code to unlock the money: C3421.')
-
-
-# In[17]:
-
-
+# Testing above function (Test 2)
 classify("Sounds good, Tom, then see u there")
 
-
-# ### Measuring the Spam Filter's Accuracy
-# The two results above look promising, but let's see how well the filter does on our test set, which has 1,114 messages.
-# 
+#### Measuring the Spam Filter's Accuracy
+# The two results above look promising, but let's see how well the filter does on our test set, which has 1,114 messages. 
 # We'll start by writing a function that returns classification labels instead of printing them.
-
-# In[18]:
-
-
 def classify_test_set(message):    
     '''
     message: a string
@@ -315,19 +207,10 @@ def classify_test_set(message):
 
 
 # Now that we have a function that returns labels instead of printing them, we can use it to create a new column in our test set.
-
-# In[19]:
-
-
 test_set['predicted'] = test_set['SMS'].apply(classify_test_set)
 test_set.head()
 
-
 # Now, we'll write a function to measure the accuracy of our spam filter to find out how well our spam filter does.
-
-# In[20]:
-
-
 correct = 0
 total = test_set.shape[0]
     
@@ -340,8 +223,6 @@ print('Correct:', correct)
 print('Incorrect:', total - correct)
 print('Accuracy:', correct/total)
 
-
 # ### Result Analysis
-# The accuracy is close to 98.74%, which is really good. Our spam filter looked at 1,114 messages that it hasn't seen in training, and classified 1,100 correctly.
-# 
-# In this project, we managed to build a spam filter for SMS messages using the multinomial Naive Bayes algorithm. The filter had an accuracy of 98.74% on the test set we used, which is a pretty good result. Our initial goal was an accuracy of over 80%, and we managed to do way better than that.
+# The accuracy is close to 98.74%, which is really good. Our spam filter looked at 1,114 messages that it hasn't seen in training, 
+# and classified 1,100 correctly.
